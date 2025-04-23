ac0ede032e7aa23af43f84a32f202a2964c65257
Gayatri780 <gayatriapatil2003@gmail.com>
2025-04-23 14:01:58 +0530

    Add files via upload

---
+++ MulDiv.java
@@ -0,0 +1,18 @@
+package Pack1;
+
+public class MulDiv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int c=a<<2;
+		int d=a>>2;
+		System.out.println(c);
+		System.out.println(d);
+
+
+	}
+
+	}
+
+
